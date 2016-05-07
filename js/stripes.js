1512757ad8ada69f510db99a30bb6c7926ec652f
ariroseonline <ariroseonline@gmail.com>
2016-05-07 04:31:04 -0400

    5

--- js/stripes.js
+++ js/stripes.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 	$('.stripe-1').css({'backgroundColor': colors[getRandomInt(0,3)]})
 	$('.stripe-2').css({'backgroundColor': colors[getRandomInt(0,3)]})
 	$('.stripe-3').css({'backgroundColor': colors[getRandomInt(0,3)]})
+	$('.stripe-4').css({'backgroundColor': colors[getRandomInt(0,3)]})
+	$('.stripe-5').css({'backgroundColor': colors[getRandomInt(0,3)]})
 	$('h1').css({'textShadow': '2px 12px 15px ' + colors[getRandomInt(0,3)]})
 	}, 800)
 });
